aa6570e09398d401b8ba64e863fb62a2ca0ce49c
willwithers-stack <will.withers@gmail.com>
2026-03-18 00:06:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ if uploaded_file:
         # --- TAB 3: RED/GREEN ZONE ---
         with tabs[3]:
             st.header("🟢 Red/Green Zone")
-            rz = p_data[p_data[cols['field']].between(1, 20)].copy()
-            gz = p_data[p_data[cols['field']].between(21, 40)].copy()
+            rz = p_data[p_data[cols['field']].between(11, 20)].copy()
+            gz = p_data[p_data[cols['field']].between(1, 10)].copy()
             c1, c2 = st.columns(2)
             with c1:
                 st.subheader("🔴 Red Zone (20-11)")
